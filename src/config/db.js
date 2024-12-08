57c3813d56e9a227afc2458b11b1f412e1310db4
deni <rumana.deni06@gmail.com>
2024-12-08 23:38:00 +0800

    initial commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
-
 const mysql = require('mysql2');
 
 const dbPool = mysql.createPool({
-    socketPath: process.env.DB_HOST,
+    socketPath: process.env.INSTANCE_UNIX_SOCKET,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
 });
 
